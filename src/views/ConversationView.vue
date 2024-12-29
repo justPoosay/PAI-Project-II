6ddd1cc9af938cd52e177fca2a216beecbc0cc56
Averi <git@averi.me>
2024-12-29 19:36:45 +0100

    Changed the background

--- src/views/ConversationView.vue
+++ src/views/ConversationView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex h-full selection:bg-white/10 text-white bg-[url('/img/DarkestHour.webp')]">
+  <div class="flex h-full selection:bg-white/10 text-white bg-[url('/img/bg/Mountain.webp')] bg-cover">
     <Suspense>
       <ConversationWrapper/>
 
