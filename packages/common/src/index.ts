114129dd4dff530362d4d92984e262c83d67cd70
Averi <git@averi.me>
2025-03-27 08:08:23 -0400

    style: fixed formatting

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,4 +1,4 @@
 export * from './models';
 export * from './schemas';
 
-export type { AppRouter } from "../../backend/src/trpc/index"
+export type { AppRouter } from '../../backend/src/trpc/index';
