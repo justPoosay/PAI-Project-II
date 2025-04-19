9644c1fbcc1c7d6d0134e55a1869b3ac9b2c9022
Averi <git@averi.me>
2025-04-19 18:47:26 +0200

    fix: improve MongoDB service handling

--- packages/backend/src/lib/db.ts
+++ packages/backend/src/lib/db.ts
@@ -1,8 +1,7 @@
 import { type, type Type } from 'arktype';
-import { logger } from 'better-auth';
-import { randomUUIDv7 } from 'bun';
 import { Conversation, Message } from 'common';
-import { MongoClient, ObjectId, type Filter, type OptionalUnlessRequiredId } from 'mongodb';
+import { MongoClient, type Filter, type OptionalUnlessRequiredId } from 'mongodb';
+import logger from './logger';
 import { env } from './utils';
 
 const mongoClient = new MongoClient(env.DATABASE_URL);
@@ -14,7 +13,7 @@ export async function closeDB() {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-function serviceFactory<S extends Type<{}, {}>>(collectionName: string, schema: S) {
+function createService<S extends Type<{}, {}>>(collectionName: string, schema: S) {
   type T = S['inferOut'];
 
   function parse(data: unknown): T | null {
@@ -24,6 +23,7 @@ function serviceFactory<S extends Type<{}, {}>>(collectionName: string, schema: 
 
     const out = schema(data);
     if (out instanceof type.errors) {
+      logger.warn('MongoDB schema validation error', collectionName, out.summary);
       return null;
     }
     return out;
@@ -32,37 +32,26 @@ function serviceFactory<S extends Type<{}, {}>>(collectionName: string, schema: 
   const collection = db.collection<T>(collectionName);
 
   return Object.freeze({
-    async findOne<U extends Filter<T>>(where: U): Promise<Extract<T, U> | null> {
+    async findOne<U extends Filter<T>>(where: U): Promise<Extract<T, Pick<U, keyof T>> | null> {
       const entity = await collection.findOne(where);
-      return parse(entity) as Extract<T, U> | null;
+      return parse(entity) as Extract<T, Pick<U, keyof T>> | null;
     },
-    async find<U extends Filter<T>>(where: U = {} as U): Promise<Extract<T, U>[]> {
+    async find<U extends Filter<T>>(where: U): Promise<Extract<T, Pick<U, keyof T>>[]> {
       const entities = await collection.find(where).toArray();
-      return entities.map(parse).filter((v): v is Extract<T, U> => v !== null);
+      return entities.map(parse).filter((v): v is Extract<T, Pick<U, keyof T>> => v !== null);
     },
-    async create<U extends T>(
-      data: Omit<U, 'id' | 'created_at' | 'updated_at'> &
-        (U extends { id: string } ? { id?: string } : { _id?: ObjectId })
-    ): Promise<U> {
-      const now = new Date();
-      const obj: any = { ...data, created_at: now, updated_at: now }; // eslint-disable-line @typescript-eslint/no-explicit-any
-
-      if (schema.extends({ id: 'string' }) && !obj.id) obj.id = randomUUIDv7();
-
-      await collection.insertOne(schema.assert(obj) as OptionalUnlessRequiredId<T>);
-      return obj as U;
+    async create<U extends T>(data: U): Promise<U> {
+      await collection.insertOne(data as unknown as OptionalUnlessRequiredId<T>);
+      return data;
     },
-    async update<
-      U extends O extends true ? T : Partial<T>,
-      F extends Filter<T>,
-      O extends boolean = false
-    >(where: F, data: U, overwrite: O = false as O): Promise<Extract<T, U & F> | null> {
-      const now = new Date();
-      const obj: typeof data & { updated_at: Date } = { ...data, updated_at: now };
-
+    async update<U extends O extends true ? T : Partial<T>, F extends Filter<T>, O extends boolean>(
+      where: F,
+      data: U,
+      overwrite: O = false as O
+    ): Promise<Extract<T, U & F> | null> {
       const value = overwrite
-        ? await collection.findOneAndReplace(where, obj, { returnDocument: 'after' })
-        : await collection.findOneAndUpdate(where, { $set: obj }, { returnDocument: 'after' });
+        ? await collection.findOneAndReplace(where, data, { returnDocument: 'after' })
+        : await collection.findOneAndUpdate(where, { $set: data }, { returnDocument: 'after' });
 
       return parse(value) as Extract<T, U & F> | null;
     },
@@ -73,7 +62,7 @@ function serviceFactory<S extends Type<{}, {}>>(collectionName: string, schema: 
   } as const);
 }
 
-export const ConversationService = serviceFactory(
+export const ConversationService = createService(
   'conversations',
   type.or(
     Conversation.and({
--- packages/backend/src/trpc/routers/conversation.ts
+++ packages/backend/src/trpc/routers/conversation.ts
@@ -3,16 +3,13 @@ import { Effort, Model } from 'common';
 import { publicProcedure, router } from '../trpc';
 
 export const conversationRouter = router({
-  delete: publicProcedure
-    .input(type({ id: 'string' }))
-    .mutation(
-      async opts =>
-        void (await opts.ctx.db.conversations.update(
-          { id: opts.input.id },
-          { id: opts.input.id, deleted: true },
-          true
-        ))
-    ),
+  delete: publicProcedure.input(type({ id: 'string' })).mutation(async opts => {
+    await opts.ctx.db.conversations.update(
+      { id: opts.input.id },
+      { id: opts.input.id, deleted: true },
+      true
+    );
+  }),
   modify: publicProcedure
     .input(
       type({
