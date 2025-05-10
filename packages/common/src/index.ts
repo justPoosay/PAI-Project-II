f09e55775da898af27c13485ad4dcd5d89e17be3
Averi <git@averi.me>
2025-05-10 19:57:16 +0200

    refactor: move backend and frontend packages to apps directory

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,5 +1,5 @@
-export type { AppRouter } from '../../backend/src/trpc/index';
+export type { AppRouter } from '../../../apps/backend/src/trpc/index';
 export * from './models';
 export * from './schemas';
 
-export type Auth = typeof import('../../backend/src/lib/auth').auth;
+export type Auth = typeof import('../../../apps/backend/src/lib/auth').auth;
